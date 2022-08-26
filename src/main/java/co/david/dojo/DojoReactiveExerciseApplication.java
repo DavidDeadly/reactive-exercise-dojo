5035092726c2f50a90764027939fce44e77d74a6
DavidDeadly <jdrueda513@gmail.com>
2022-08-26 07:01:20 -0500

    Initial commit

---
+++ src/main/java/co/david/dojo/DojoReactiveExerciseApplication.java
@@ -0,0 +1,9 @@
+package co.david.dojo;
+
+public class DojoReactiveExerciseApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ src/test/java/co/david/dojo/DojoReactiveExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package co.david.dojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoReactiveExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
